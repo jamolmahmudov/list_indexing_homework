ba4b9cfd9c2a324d50c1ae453659760034a1be84
jamolmahmudov <jamolmahmudov27102003@gmail.com>
2023-04-26 11:26:24 +0500

    7

--- List07.py
+++ List07.py
@@ -7,16 +7,21 @@ def main(list1):
         list: return answer
     """
  
-    if list1[0]==0:
-        list1[0]=False
-    if list1[1]==0:
-        list1[1]=False
-    if list1[2]==0:
-        list1[2]=False
-    if list1[3]==0:
-        list1[3]=False
-    if list1[4]==0:
-        list1[4]=False
-    
+    # if list1[0]==0:
+    #     list1[0]=False
+    # if list1[1]==0:
+    #     list1[1]=False
+    # if list1[2]==0:
+    #     list1[2]=False
+    # if list1[3]==0:
+    #     list1[3]=False
+    # if list1[4]==0:
+    #     list1[4]=False
+    i=0
+    while i<len (list1):
+        if list1[i]==0:
+            list1[i]=False
+        
+        i+=1
     return list1
 print(main([1,1,0,0,1]))
